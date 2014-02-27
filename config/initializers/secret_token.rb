2c1eea2ef8edd8ce0c92192f8b480093a167282f
barache <barache.nadia@gmail.com>
2014-02-27 12:48:33 +0100

    initial import carrierwave

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testcarrierwave::Application.config.secret_key_base = '19bc6efde7403d2259a442f4880eaa0c655d488c29107de14b1cb9742d6c8f8db62b0a1942226e3e8adc1e3e54b56ea962fbf07dc68050451bfb81661294c80c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testcarrierwave::Application.config.session_store :cookie_store, key: '_testcarrierwave_session'
