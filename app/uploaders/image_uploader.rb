00852553b76b8c18ccc49bdd3fdb6094b5946b6e
barache <barache.nadia@gmail.com>
2014-02-27 12:53:13 +0100

    add uploader index

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,5 @@
-# encoding: utf-8
-
 class ImageUploader < CarrierWave::Uploader::Base
-
+ include CarrierWave::RMagick
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -15,7 +13,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+version :thumb do
+    process :resize_to_limit => [500,500]
+  end
+  def extension_white_list
+     %w(jpg jpeg png pdf)
+end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
